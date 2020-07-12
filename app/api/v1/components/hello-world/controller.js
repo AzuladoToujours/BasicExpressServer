13355ab9b9b1f9a0a523f811f5ac1b8fc176521c
Camilo <camiloa.mejia@udea.edu.co>
2020-07-11 19:15:53 -0500

    no GET

--- app/api/v1/components/hello-world/controller.js
+++ app/api/v1/components/hello-world/controller.js
@@ -2,29 +2,28 @@ const httpStatus = require('http-status');
 const util = require('./hello');
 require('dotenv').config();
 
-const get = async (req, res) => {
-  try {
-    const ans = util.sayHello();
+// const get = async (req, res) => {
+//   try {
+//     const ans = util.sayHello();
 
-    if (!ans) {
-      return res
-        .status(httpStatus.NOT_FOUND)
-        .send({ message: 'Not found' });
-    }
-    return res
-      .status(httpStatus.OK)
-      .send(ans);
-  } catch (error) {
-    console.error(error);
-    return res
-      .status(httpStatus.INTERNAL_SERVER_ERROR)
-      .send({ message: 'Interal server error' });
-  }
-};
+//     if (!ans) {
+//       return res
+//         .status(httpStatus.NOT_FOUND)
+//         .send({ message: 'Not found' });
+//     }
+//     return res
+//       .status(httpStatus.OK)
+//       .send(ans);
+//   } catch (error) {
+//     console.error(error);
+//     return res
+//       .status(httpStatus.INTERNAL_SERVER_ERROR)
+//       .send({ message: 'Interal server error' });
+//   }
+// };
 
 const testing = async (req, res) => res.status(200).send(`Hello from ${process.env.HOST}`);
 
 module.exports = {
-  get,
   testing,
 };
